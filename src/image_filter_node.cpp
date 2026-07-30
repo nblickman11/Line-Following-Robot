55274b6e111871125b888ba2e76b8e5b20960ca9
Nelson <62907195+nblickman11@users.noreply.github.com>
2026-07-29 20:57:19 -0400

    Update image_filter_node.cpp

--- src/image_filter_node.cpp
+++ src/image_filter_node.cpp
@@ -18,7 +18,7 @@ public:
     publisher_ = this->create_publisher<sensor_msgs::msg::Image>(
       "/filtered/image_raw", 10);
 
-    RCLCPP_INFO(this->get_logger(), "ImageFilterNode started b.");
+    RCLCPP_INFO(this->get_logger(), "ImageFilterNode started.");
   }
 
 private:
@@ -29,21 +29,16 @@ private:
       // Convert ROS Image to OpenCV Mat (BGR8)
       cv::Mat cv_image = cv_bridge::toCvCopy(msg, "bgr8")->image;
 
-    // ROI: bottom slice of image
-    //int height = cv_image.rows;
-      //cv::Mat roi = cv_image(cv::Range(height - 50, height), cv::Range::all());
+      // TODO: Evaluate cropping to a lower image ROI to reduce computation
+      // and improve robustness when only the line near the robot is needed.
 
       // Convert to grayscale
       cv::Mat gray;
       cv::cvtColor(cv_image, gray, cv::COLOR_BGR2GRAY);
 
-double minVal, maxVal;
-cv::minMaxLoc(gray, &minVal, &maxVal);
-RCLCPP_INFO(this->get_logger(), "Grayscale intensity: min = %.1f, max = %.1f", minVal, maxVal);
-
-    // Threshold to isolate white areas
-    cv::Mat binary;
-    cv::threshold(gray, binary, 140, 255, cv::THRESH_BINARY);
+      // Threshold to isolate white areas
+      cv::Mat binary;
+      cv::threshold(gray, binary, 140, 255, cv::THRESH_BINARY);
 
       // Convert back to ROS Image message (mono8)
       auto filtered_msg = cv_bridge::CvImage(msg->header, "mono8", binary).toImageMsg();
